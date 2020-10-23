1186ad92c9e100c4285115e2011c78e955e29202
mrToshkin <toshkin.na@gmail.com>
2020-10-23 21:56:26 +0400

    update

--- src/components/Calendar/Day.js
+++ src/components/Calendar/Day.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Modal from '../Modal/Modal';
 
 function Day(props) {
+
   return (
     props.days.map((week) => {
       return week.map((day) => {
         return (
-          <div className="calendar__day">
+          <div className="calendar__day" onClick={() => <Modal/>}>
             <p>{day}</p>
           </div>
         )
