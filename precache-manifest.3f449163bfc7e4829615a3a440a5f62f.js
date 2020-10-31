980536be82abdee44bca49b7b390fb70299819e5
mrToshkin <toshkin.na@gmail.com>
2020-11-01 01:04:42 +0400

    gh-pages

---
+++ precache-manifest.3f449163bfc7e4829615a3a440a5f62f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5965dcb7c3d581f650253c98d288bad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84d7a622fb111966a34b",
+    "url": "/static/css/main.b937b2e0.chunk.css"
+  },
+  {
+    "revision": "8fd71fa4fd3340b2c439",
+    "url": "/static/js/2.76a3d4a2.chunk.js"
+  },
+  {
+    "revision": "7ab528fdfd6665734e2ba0ae9acdffb4",
+    "url": "/static/js/2.76a3d4a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84d7a622fb111966a34b",
+    "url": "/static/js/main.3d5928dc.chunk.js"
+  },
+  {
+    "revision": "8a433b15c6740c0b6781",
+    "url": "/static/js/runtime-main.fc8ac5dc.js"
+  }
+]);
--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Calendar from './components/Calendar/Calendar.jsx';
-import 'focus-visible';
-
-function App() {
-  return <Calendar />
-}
-
-export default App;
